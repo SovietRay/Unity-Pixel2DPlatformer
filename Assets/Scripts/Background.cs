43f436ed730c17adcb0bcad5af6d8b69130ae793
Mikhail Klishevich <x-ray@mail.ru>
2020-03-01 20:42:10 +0700

    New lvl, work with background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] Transform _player;
+    void Start()
+    {
+        //Debug.Log(transform.position.y + "!!!");
+        transform.position = new Vector3(gameObject.transform.position.x, _player.position.y+2f, gameObject.transform.position.z);
+    }
+    private void LateUpdate()
+    {
+        //Debug.Log(_player.position.y);
+        Debug.Log(transform.position.y + "!!!");
+        transform.position = new Vector3(gameObject.transform.position.x, _player.position.y + 2f, gameObject.transform.position.z);
+    }
+}
